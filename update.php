b954ffadb8e07e89b196d6fbd24077d52578aba1
AravindanSridhar <aravindansridhars@gmail.com>
2019-05-15 09:43:25 +0530

    db variable update

--- update.php
+++ update.php
@@ -3,22 +3,21 @@
 
 require_once 'init.php';
 
-if(isset($_POST["id"]))
-{
- $query = "
+if (isset($_POST["id"])) {
+    $query = "
  UPDATE events 
  SET title=:title,start_date=:start_date, end_date=:end_date 
  WHERE id=:id
  ";
- $statement = $connect->prepare($query);
- $statement->execute(
-  array(
-   ':title'  => $_POST['title'],
-   ':start_date' => $_POST['start'],
-   ':end_date' => $_POST['end'],
-   ':id'   => $_POST['id']
-  )
- );
+    $statement = $db->prepare($query);
+    $statement->execute(
+        array(
+            ':title'  => $_POST['title'],
+            ':start_date' => $_POST['start'],
+            ':end_date' => $_POST['end'],
+            ':id'   => $_POST['id']
+        )
+    );
 }
 
 ?>
