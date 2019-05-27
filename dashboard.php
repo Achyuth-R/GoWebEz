455afa175fe8728ec2287c31632ef2c79b1fe387
AravindanSridhar <aravindansridhars@gmail.com>
2019-05-27 15:16:29 +0530

    removed unncessary  checkbox

--- dashboard.php
+++ dashboard.php
@@ -13,115 +13,109 @@ if ($_SESSION['position'] == 'admin') {
          <table class="table  tableSectionBox table-hover">
             <thead class="text-left default-cursor">
                <tr>
-                  <th class="text-muted tableSectionBoxCheckAll">
-                     <input class="m-2 checkall" type="checkbox" id="checkall">
-                  </th>
                   <th class="text-muted">
-                     <span><i class="fa fa-user-o ml-2 mr-2" aria-hidden="true"></i> Name</span>
+                     <span><i class="fas fa-user ml-2 mr-2" aria-hidden="true"></i> Name</span>
                   </th>
-                  <th class="text-muted"><i class="fa fa-pen-o mr-1" aria-hidden="true"></i>Designation</th>
-                  <th class="text-muted"><i class="fa fa-envelope-o mr-1" aria-hidden="true"></i> E-mail</th>
-                  <th class="text-muted"><i class="fa fa-calendar mr-1" aria-hidden="true"></i> Date of Application</th>
-                  <form class="form" id="filter">   
-                  <th class="text-muted"><i class="fa fa-line-chart mr-1" aria-hidden="true"></i>
-                              
-
-  <button class="select btn" type="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" style="color:grey;">
-    <b>Status<b><i class="fa fa-caret-down" aria-hidden="true"></i>
-  </button>
-  <div class="dropdown-menu">
-  <option class="dropdown-item dropdown_filter" value="" href="#">All</option>
-  <option class="dropdown-item dropdown_filter" value="~" href="#">Status Pending</option>
-  <option class="dropdown-item dropdown_filter" value="Accepted" href="#">Accepted</option>
-  <option class="dropdown-item dropdown_filter" value="Selected" href="#">Selected</option>
-  <option class="dropdown-item dropdown_filter" value="Rejected" href="#">Rejected</option>
-  
-  </div>
-</div>
-</form>
+                  <th class="text-muted"><i class="fas fa-pen mr-1" aria-hidden="true"></i>Designation</th>
+                  <th class="text-muted"><i class="fas fa-envelope mr-1" aria-hidden="true"></i> E-mail</th>
+                  <th class="text-muted"><i class="fas fa-calendar mr-1" aria-hidden="true"></i> Date of Application</th>
+                  <form class="form" id="filter">
+                     <th class="text-muted"><i class="fas fa-line-chart mr-1" aria-hidden="true"></i>
 
 
+                        <button class="select btn" type="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" style="color:grey;">
+                           <b>Status<b><i class="fa fa-caret-down" aria-hidden="true"></i>
+                        </button>
+                        <div class="dropdown-menu">
+                           <option class="dropdown-item dropdown_filter" value="" href="#">All</option>
+                           <option class="dropdown-item dropdown_filter" value="~" href="#">Status Pending</option>
+                           <option class="dropdown-item dropdown_filter" value="Accepted" href="#">Accepted</option>
+                           <option class="dropdown-item dropdown_filter" value="Selected" href="#">Selected</option>
+                           <option class="dropdown-item dropdown_filter" value="Rejected" href="#">Rejected</option>
+
+                        </div>
+      </div>
+      </form>
 
 
 
 
-               </tr>
-            </thead>
-            <tbody class="text-left" id="candidate_list">
-               <?php
-               $select_candidate_list = "SELECT * FROM registered_user";
-               $select_candidate_list = $db->query($select_candidate_list);
 
 
-               while ($rows = $select_candidate_list->fetch(PDO::FETCH_OBJ)) {
-                  if ($rows->fresher_or_experienced == "Fresher") {
-                     $color = "warning";
-                  } else if ($rows->fresher_or_experienced == "Experienced") {
-                     $color = "dark";
-                  } else {
-                     $color = "info";
-                  }
-                  ?>
+      </tr>
+      </thead>
+      <tbody class="text-left" id="candidate_list">
+         <?php
+         $select_candidate_list = "SELECT * FROM registered_user";
+         $select_candidate_list = $db->query($select_candidate_list);
+
+
+         while ($rows = $select_candidate_list->fetch(PDO::FETCH_OBJ)) {
+            if ($rows->fresher_or_experienced == "Fresher") {
+               $color = "warning";
+            } else if ($rows->fresher_or_experienced == "Experienced") {
+               $color = "dark";
+            } else {
+               $color = "info";
+            }
+            ?>
+
+            <?php
+            if ($rows->del_flag == 'D')
+               $status_class = 'white';
+            else
+               $status_class = 'gray_clr';
+            ?>
+            <tr class="<?php echo $status_class ?>" data-id="<?php echo $rows->id ?>">
+               <input type="hidden" class="candidate_id" name="id" value="<?php echo $rows->id ?>">
+               <td class="align-middle">
+
+                  <img class="rounded-circle img-fluid mr-3 tableprofileImg float-left" src="assets/images/img1.jpg">
+                  <p class="align-middle text-xm-center"><?php echo $rows->name ?><span class="text-primary default-cursor font-weight-bold">
+                        <br>( <?php echo $rows->qualification ?> )</span>
+                  </p>
+               </td>
+               <td class="align-middle">
+                  <div class="<?php echo $color ?> default-cursor btn-sm"><?php echo $rows->fresher_or_experienced; ?></div>
+               </td>
+               <td class="tableSectionEmail  align-middle"><?php echo $rows->email ?></td>
+               <td class="align-middle">11/12/2014</td>
+               <td class="spinner default-cursor align-middle">
+
+
 
                   <?php
-                  if ($rows->del_flag == 'D')
-                     $status_class = 'white';
-                  else
-                     $status_class = 'gray_clr';
-                  ?>
-                  <tr class="<?php echo $status_class ?>" data-id="<?php echo $rows->id ?>">
-                     <input type="hidden" class="candidate_id" name="id" value="<?php echo $rows->id ?>">
-                     <td class="tablelSectionTdUl align-middle p-0">
-                        <input class="mx-3 align-middle checkbox-child" type="checkbox">
-                     </td>
-                     <td class="align-middle">
 
-                        <img class="rounded-circle img-fluid mr-3 tableprofileImg float-left" src="assets/images/img1.jpg">
-                        <p class="align-middle text-xm-center"><?php echo $rows->name ?><span class="text-primary default-cursor font-weight-bold">
-                              <br>( <?php echo $rows->qualification ?> )</span>
-                        </p>
-                     </td>
-                     <td class="align-middle">
-                        <div class="<?php echo $color ?> default-cursor btn-sm"><?php echo $rows->fresher_or_experienced; ?></div>
-                     </td>
-                     <td class="tableSectionEmail  align-middle"><?php echo $rows->email ?></td>
-                     <td class="align-middle">11/12/2014</td>
-                     <td class="spinner default-cursor align-middle">
+                  if ($rows->del_flag == 'D') {
 
-
-
-                        <?php
-
-                        if ($rows->del_flag == 'D') {
-
-                           echo '<div class="spinner-grow text-success table-spinner ml-2 ">~</div>
+                     echo '<div class="spinner-grow text-success table-spinner ml-2 ">~</div>
                         <div class="spinner-grow text-info table-spinner"></div>
                         <div class="spinner-grow text-warning table-spinner"></div>
                         <div class="spinner-grow text-danger table-spinner"></div>
                         <div class="spinner-grow text-secondary table-spinner"></div>
                         <div class="spinner-grow text-danger table-spinner"></div>';
-                        } else {
+                  } else {
 
-                           if ($rows->del_flag == 'Accepted')
-                              $status_update = 'btn-info';
-                           if ($rows->del_flag == 'Rejected')
-                              $status_update = 'btn-danger';
-                           if ($rows->del_flag == 'Selected')
-                              $status_update = 'btn-success';
+                     if ($rows->del_flag == 'Accepted')
+                        $status_update = 'btn-info';
+                     if ($rows->del_flag == 'Rejected')
+                        $status_update = 'btn-danger';
+                     if ($rows->del_flag == 'Selected')
+                        $status_update = 'btn-success';
 
-                           echo '<span class="btn-sm default-cursor font-weight-bold ' . $status_update . '"> ' . $rows->del_flag . '</span>';
-                        }
+                     echo '<span class="btn-sm default-cursor font-weight-bold ' . $status_update . '"> ' . $rows->del_flag . '</span>';
+                  }
 
-                        ?>
-
-
+                  ?>
 
 
-                     </td>
-                  </tr>
-               <?php } ?>
-            </tbody>
-         </table>
+
+
+               </td>
+            </tr>
+         <?php } ?>
+      </tbody>
+      </table>
       </div>
    </section>
 
